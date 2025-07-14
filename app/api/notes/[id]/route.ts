c0c67b23866d7a04ba17a344fc9ca7a02508a9a2
Andriy Romanov <mgm.agro04@gmail.com>
2025-07-14 15:46:52 +0300

    edit strukture

--- app/api/notes/[id]/route.ts
+++ app/api/notes/[id]/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+/** import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { api } from "../../api";
 import { parse } from "cookie";
@@ -41,4 +41,75 @@ export async function GET() {
     }
   }
   return NextResponse.json({ success: false });
+}*/
+import { NextResponse } from "next/server";
+import { api } from "../../api";
+import { cookies } from "next/headers";
+
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+export async function GET(request: Request, { params }: Props) {
+  const cookieStore = await cookies();
+  const { id } = await params;
+  const { data } = await api(`/notes/${id}`, {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+  });
+  if (data) {
+    return NextResponse.json(data);
+  }
+  return NextResponse.json({ error: "Failed to fetch note" }, { status: 500 });
+}
+
+export async function DELETE(request: Request, { params }: Props) {
+  const cookieStore = await cookies();
+  const { id } = await params;
+
+  try {
+    await api.delete(`/notes/${id}`, {
+      headers: {
+        Cookie: cookieStore.toString(),
+      },
+    });
+    return NextResponse.json(
+      { message: "Note deleted successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error deleting note:", error);
+    return NextResponse.json(
+      { error: "Failed to delete note" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PATCH(request: Request, { params }: Props) {
+  const cookieStore = await cookies();
+  const { id } = await params;
+  const body = await request.json();
+
+  try {
+    const { data } = await api.patch(`/notes/${id}`, body, {
+      headers: {
+        Cookie: cookieStore.toString(),
+      },
+    });
+    if (data) {
+      return NextResponse.json(data);
+    }
+    return NextResponse.json(
+      { error: "Failed to update note" },
+      { status: 500 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "Failed to update note" },
+      { status: 500 }
+    );
+  }
 }

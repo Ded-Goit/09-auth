ca44157c2ffef5fcd0610f3a693dabd335ba40c9
Andriy Romanov <mgm.agro04@gmail.com>
2025-07-17 22:41:20 +0300

    edit  api

--- app/api/auth/session/route.ts
+++ app/api/auth/session/route.ts
@@ -1,40 +1,90 @@
 import { NextResponse } from "next/server";
-import { cookies } from "next/headers";
 import { api } from "../../api";
-import { parse } from "cookie";
+import { cookies } from "next/headers";
+import { logErrorResponse } from "../../_utils/utils";
+import { isAxiosError } from "axios";
 
-export async function GET() {
-  const cookieStore = await cookies();
-  const accessToken = cookieStore.get("accessToken")?.value;
-  const refreshToken = cookieStore.get("refreshToken")?.value;
+type Props = {
+  params: Promise<{ id: string }>;
+};
 
-  if (accessToken) {
-    return NextResponse.json({});
-  }
-
-  if (refreshToken) {
-    const apiRes = await api.get("auth/session", {
+export async function GET(request: Request, { params }: Props) {
+  try {
+    const cookieStore = await cookies();
+    const { id } = await params;
+    const res = await api(`/notes/${id}`, {
       headers: {
         Cookie: cookieStore.toString(),
       },
     });
-    const setCookie = apiRes.headers["set-cookie"];
-    if (setCookie) {
-      const cookieArray = Array.isArray(setCookie) ? setCookie : [setCookie];
-      let accessToken = "";
-      let refreshToken = "";
-
-      for (const cookieStr of cookieArray) {
-        const parsed = parse(cookieStr);
-        if (parsed.accessToken) accessToken = parsed.accessToken;
-        if (parsed.refreshToken) refreshToken = parsed.refreshToken;
-      }
-
-      if (accessToken) cookieStore.set("accessToken", accessToken);
-      if (refreshToken) cookieStore.set("refreshToken", refreshToken);
-
-      return NextResponse.json({});
+    return NextResponse.json(res.data, { status: res.status });
+  } catch (error) {
+    if (isAxiosError(error)) {
+      logErrorResponse(error.response?.data);
+      return NextResponse.json(
+        { error: error.message, response: error.response?.data },
+        { status: error.status }
+      );
     }
+    logErrorResponse({ message: (error as Error).message });
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
   }
-  return NextResponse.json({});
+}
+
+export async function DELETE(request: Request, { params }: Props) {
+  try {
+    const cookieStore = await cookies();
+    const { id } = await params;
+
+    const res = await api.delete(`/notes/${id}`, {
+      headers: {
+        Cookie: cookieStore.toString(),
+      },
+    });
+    return NextResponse.json(res.data, { status: res.status });
+  } catch (error) {
+    if (isAxiosError(error)) {
+      logErrorResponse(error.response?.data);
+      return NextResponse.json(
+        { error: error.message, response: error.response?.data },
+        { status: error.status }
+      );
+    }
+    logErrorResponse({ message: (error as Error).message });
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PATCH(request: Request, { params }: Props) {
+  try {
+    const cookieStore = await cookies();
+    const { id } = await params;
+    const body = await request.json();
+
+    const res = await api.patch(`/notes/${id}`, body, {
+      headers: {
+        Cookie: cookieStore.toString(),
+      },
+    });
+    return NextResponse.json(res.data, { status: res.status });
+  } catch (error) {
+    if (isAxiosError(error)) {
+      logErrorResponse(error.response?.data);
+      return NextResponse.json(
+        { error: error.message, response: error.response?.data },
+        { status: error.status }
+      );
+    }
+    logErrorResponse({ message: (error as Error).message });
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
